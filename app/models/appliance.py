8723e80c9f1998448f4e4fa55e6051f143ca954b
Andrea <andreagalindo.guerra@gmail.com>
2023-05-25 16:07:26 -0400

    Adds appliance model methods

--- app/models/appliance.py
+++ app/models/appliance.py
@@ -5,3 +5,24 @@ class Appliance(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     power_consumption_watts = db.Column(db.Integer)
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(name=data_dict["name"], power_consumption_watts=data_dict["power_consumption_watts"])
+
+    def to_dict(self):
+        appliance_dict = {
+            "id": self.id,
+            "name": self.name,
+            "power_consumption_watts": self.power_consumption_watts,
+        }
+
+        return appliance_dict
+
+    def update(self, req_body):
+        try:
+            self.name = req_body["name"]
+            self.power_consumption_watts = req_body["power_consumption_watts"]
+        except KeyError:
+            abort(make_response(jsonify(dict(details="Invalid data")), 400))
+            
